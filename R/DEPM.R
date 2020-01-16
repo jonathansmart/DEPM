364216452f3f1f11128f1a607d88e3a7d076e89f
Jonathan Smart <SMARTJ20@pirsa.sa.gov.au>
2020-01-16 15:19:17 +1030

    All package files cleaned up to remove all warnings. Only remaining note alerts that the cpp files are > 1mb, which is fine.

--- R/DEPM.R
+++ R/DEPM.R
@@ -5,3 +5,10 @@
 #' @description Estimate spawning biomass for a fish population using the Daily Egg Production Method
 #' @docType package
 NULL
+
+utils::globalVariables(c( ".", "Age", "Bottom", "CV", "Fecundity", "Mean_F", "Mean_W", "P",
+                          "P0_se", "Parameter", "Predicted", "nlminb", 'na.omit', 'sd',
+                          'cov', 'var','Ratio estimate',"funs", "group_cols", 'n', "row_number", "vars",
+                          "Prop", "Pt", "Ratio", "estimate", "Region", "SE",
+                          "Time", "Top", "Var", "Variance", "Wt", "Wt_bin", "Z",
+                          "head", "var_F", "var_W"))
